ce223fa57fba2db60d4d8b940e7c07baa77e2acd
kiilii <tonight17@126.com>
2022-03-27 17:47:48 +0800

    ✨ feat(开发环境): 新增 golang 开发环境
    设置goproxy, 下载 protobuf

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function golang () {
+    # 设置代理
+    go env -w GO111MODULE=on
+    go env -w GOPROXY=https://goproxy.cn,direct
+
+    # 安装 protobuf
+    apt-get update
+    apt-get install -y gogoprotobuf
+}
+
+hello () {
+    echo "hello world"
+}
+
+$@
